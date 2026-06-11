20165ba159313ce3d38c09008bc422cbd9060be8
LXTimer <106882743+LXTimer@users.noreply.github.com>
2026-06-11 11:04:15 +0800

    Add files via upload

---
+++ src/piece/King.java
@@ -0,0 +1,51 @@
+package piece;
+
+import view.GamePanel;
+
+public class King extends Piece {
+
+    public King(int col, int row, int color) {
+        super(col, row, color);
+        this.type = "KING";
+        this.img = getImage("/resources/" + (color == 0 ? "white" : "black") + "-king");
+    }
+
+    @Override
+    public boolean canMove(int targetCol, int targetRow) {
+        if (!isWithinBoard(targetCol, targetRow) || isSameSquare(targetCol, targetRow)) {
+            return false;
+        }
+
+        int colDiff = Math.abs(targetCol - preCol);
+        int rowDiff = Math.abs(targetRow - preRow);
+
+        if (colDiff <= 1 && rowDiff <= 1) {
+            return isValidSquare(targetCol, targetRow);
+        }
+
+        if (!moved && rowDiff == 0 && colDiff == 2) {
+            return canCastle(targetCol);
+        }
+
+        return false;
+    }
+
+    private boolean canCastle(int targetCol) {
+        int rookCol = targetCol > preCol ? 7 : 0;
+        int direction = targetCol > preCol ? 1 : -1;
+        Piece rook = getPieceAt(rookCol, preRow);
+
+        if (rook == null || rook.color != color || !"ROOK".equals(rook.type) || rook.moved) {
+            return false;
+        }
+
+        for (int col = preCol + direction; col != rookCol; col += direction) {
+            if (getPieceAt(col, preRow) != null) {
+                return false;
+            }
+        }
+
+        GamePanel.castlingP = rook;
+        return true;
+    }
+}
---
+++ src/piece/Piece.java
@@ -0,0 +1,191 @@
+package piece;
+
+import model.Board;
+import view.GamePanel;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Objects;
+
+public class Piece {
+
+    // Declare essential attributes for a chess piece
+    public String type;
+    public BufferedImage img;
+    public int x, y;
+    public int col, row, preCol, preRow;
+    public int color;
+    public Piece hittingP;
+    public boolean moved, twoStepped;
+
+    // Constrcutor
+    public Piece(int col, int row, int color) {
+        this.col = col;
+        this.row = row;
+        this.color = color;
+        this.preCol = col;
+        this.preRow = row;
+        updatePixelPosition();
+    }
+
+    // Method to load piece images
+    public BufferedImage getImage(String path) {
+        try {
+            return ImageIO.read(
+                    Objects.requireNonNull(Piece.class.getResourceAsStream(path + ".png")));
+        } catch (Exception e) {
+            System.err.println("Failed to load image: " + path);
+            return null;
+        }
+    }
+
+    // Methods to convert between board coordinates and pixel coordinates
+    public int getX(int col) {
+        return col * Board.SIZE;
+    }
+
+    public int getY(int row) {
+        return row * Board.SIZE;
+    }
+
+    public int getCol(int x) {
+        return (x + Board.SIZE / 2) / Board.SIZE;
+    }
+
+    public int getRow(int y) {
+        return (y + Board.SIZE / 2) / Board.SIZE;
+    }
+
+    private void updatePixelPosition() {
+        this.x = getX(col);
+        this.y = getY(row);
+    }
+
+    public Piece getPieceAt(int targetCol, int targetRow) {
+        return GamePanel.simPieces.stream()
+                .filter(p -> p.col == targetCol && p.row == targetRow && p != this)
+                .findFirst()
+                .orElse(null);
+    }
+
+    // Method to check if there's a piece at the target position
+    public Piece getHittingP(int targetCol, int targetRow) {
+        return getPieceAt(targetCol, targetRow);
+    }
+
+    public int getIndex() {
+        for (int i = 0; i < GamePanel.simPieces.size(); i++) {
+            if (GamePanel.simPieces.get(i) == this) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void draw(Graphics2D g2) {
+        if (img != null) {
+            g2.drawImage(img, x, y, Board.SIZE, Board.SIZE, null);
+        }
+    }
+
+    public void updatePosition() {
+        if ("PAWN".equals(type) && Math.abs(row - preRow) == 2) {
+            twoStepped = true;
+        }
+
+        updatePixelPosition();
+        preCol = col;
+        preRow = row;
+        moved = true;
+    }
+
+    public boolean canMove(int targetCol, int targetRow) {
+        return false;
+    }
+
+    public boolean isWithinBoard(int targetCol, int targetRow) {
+        return targetCol >= 0 && targetCol <= 7 && targetRow >= 0 && targetRow <= 7;
+    }
+
+    public void resetPosition() {
+        col = preCol;
+        row = preRow;
+        updatePixelPosition();
+    }
+
+    public boolean isValidSquare(int targetCol, int targetRow) {
+        hittingP = getHittingP(targetCol, targetRow);
+
+        if (hittingP == null) {
+            return true;
+        }
+
+        if (hittingP.color != this.color) {
+            return true;
+        }
+
+        hittingP = null;
+        return false;
+    }
+
+    public boolean isSameSquare(int targetCol, int targetRow) {
+        return targetCol == preCol && targetRow == preRow;
+    }
+
+    public boolean pieceIsOnStraightLine(int targetCol, int targetRow) {
+        int colDirection = Integer.compare(targetCol, preCol);
+        int rowDirection = Integer.compare(targetRow, preRow);
+
+        if (colDirection != 0 && rowDirection != 0) {
+            return false;
+        }
+
+        int currentCol = preCol + colDirection;
+        int currentRow = preRow + rowDirection;
+
+        while (currentCol != targetCol || currentRow != targetRow) {
+            for (Piece p : GamePanel.simPieces) {
+                if (p.col == currentCol && p.row == currentRow) {
+                    hittingP = p;
+                    return true;
+                }
+            }
+            currentCol += colDirection;
+            currentRow += rowDirection;
+        }
+
+        return false;
+    }
+
+    public boolean pieceIsOnDiagonalLine(int targetCol, int targetRow) {
+        int colDirection = Integer.compare(targetCol, preCol);
+        int rowDirection = Integer.compare(targetRow, preRow);
+
+        if (Math.abs(targetCol - preCol) != Math.abs(targetRow - preRow)) {
+            return false;
+        }
+
+        int currentCol = preCol + colDirection;
+        int currentRow = preRow + rowDirection;
+
+        while (currentCol != targetCol && currentRow != targetRow) {
+            for (Piece p : GamePanel.simPieces) {
+                if (p.col == currentCol && p.row == currentRow) {
+                    hittingP = p;
+                    return true;
+                }
+            }
+            currentCol += colDirection;
+            currentRow += rowDirection;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String colorName = (color == 0) ? "White" : "Black";
+        return colorName + " " + type;
+    }
+}
